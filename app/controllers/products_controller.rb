ab0f85f661214395a42c3299b744471cbbef952f
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-28 20:16:57 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,15 @@ class ProductsController < ApplicationController
     render plain: product.inventory > 0 ? true : false
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def data 
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
   def description
     product = Product.find(params[:id])
     render plain: product.description
