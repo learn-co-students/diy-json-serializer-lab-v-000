cdcddef2fafd9a055c788777d105b8a487ffc22a
Brett <brettrheenan@gmail.com>
2016-04-07 16:07:04 +0200

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,21 +1,25 @@
 class ProductSerializer
 
-	def self.serialize(product)
-		{
-			"name" => product.name,
-			"price" => product.price,
-			"inventory" => product.inventory,
-			"description" => product.description
-		}.to_json
-	end
+# 	def self.serialize(product)
+# 		{
+# 			"name" => product.name,
+# 			"price" => product.price,
+# 			"inventory" => product.inventory,
+# 			"description" => product.description
+# 		}.to_json
+# 	end
+
+
+
+ def self.serialize(product)
+    serialized_product = '{'
+
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"price": "' + product.price.to_s + '"'
+
+    serialized_product += '}'
+  end
 end
-
-
-		  # create_table "products", force: :cascade do |t|
-    # t.string   "name"
-    # t.integer  "price"
-    # t.datetime "created_at",  null: false
-    # t.datetime "updated_at",  null: false
-    # t.integer  "inventory"
-    # t.string   "description"
-
