4f4d56e46775bcb219e3c244b58fb8eb25e58d08
kylegraydev <kylegraydev@gmail.com>
2016-06-14 18:21:52 -0700

    Done.

---
+++ app/models/product_serializer.rb
@@ -0,0 +1,10 @@
+class ProductSerializer
+  def self.serialize
+    endstr = '{'
+    endstr += '"name": "' + product.name + '", '
+    endstr += '"description": "' + product.description + '", '
+    endstr += '"inventory": ' + product.inventory.to_s + ', '
+    endstr += '"price": ' + product.price.to_s
+    endstr += '}'
+  end
+end
