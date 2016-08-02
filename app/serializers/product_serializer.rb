a9c8246b3f23838624c3909aa790ce1f5e5c0e08
cassaram09 <cassaram09@gmail.com>
2016-08-01 22:47:59 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,11 +1,11 @@
 class ProductSerializer
   def self.serialize(product)
-    serialized_post = '{'
-    serialized_post += '"id": ' + product.id.to_s + ', '
-    serialized_post += '"name": "' + product.name + '", '
-    serialized_post += '"price": "' + product.price.to_s + '", ' 
-    serialized_post += '"inventory": "' + product.inventory.to_s + '", '
-    serialized_post += '"description":  "' + product.description + '"'
-    serialized_post += '}'
+    serialized_product = '{'
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"price": "' + product.price.to_s + '"'
+    serialized_product += '}'
   end
 end 
