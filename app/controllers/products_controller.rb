035b54a79926ce5ce59e5753363fe756accb019a
Ray Mulligan <rmullig2@optonline.net>
2017-05-29 15:47:18 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,17 @@ class ProductsController < ApplicationController
     Product.create(product_params)
     redirect_to products_path
   end
+  
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+    #render json: ProductSerializer.serialize(product)
+    #binding.pry
+  end
 
   private
 
