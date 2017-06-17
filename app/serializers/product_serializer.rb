db320e7dad416c6e942754ed5b7f92d652f807af
Bob Habig <rfhab2@gmail.com>
2017-06-17 15:55:32 -0400

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,8 +6,10 @@ class ProductSerializer
       serialized_product += '"id": ' + product.id.to_s + ','
       serialized_product += '"name": "' + product.name + '",'
       serialized_product += '"price": ' + product.price.to_s + ','
+    if product.inventory && product.description
       serialized_product += '"inventory": ' + product.inventory.to_s + ','
       serialized_product += '"description": "' + product.description + '"'
+    end
       #close json object
       serialized_product += '}'
   end
