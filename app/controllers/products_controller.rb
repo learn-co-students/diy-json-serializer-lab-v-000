6fb2f1de98075c4bb47833b254bf994966e6c5a1
LS-C <suzy.cho111@gmail.com>
2017-11-23 10:52:35 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,10 @@ class ProductsController < ApplicationController
     render plain: product.description
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
@@ -22,9 +26,29 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def edit
+    product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    if product.update_attributes(product_params)
+      redirect_to products_path
+    end
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
+  def set_product
+    product = Product.find(params[:id])
   end
 end
