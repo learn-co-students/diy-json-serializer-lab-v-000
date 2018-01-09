ccb6fc5648a17c899eee044f3b40e8fdd1e917b0
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-09 19:56:40 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :current_product, only: [:show, :data]
+
   def index
     @products = Product.all
+  end
+
+  def show
+
   end
 
   def inventory
@@ -22,7 +28,15 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def data
+    render json: "#{ProductSerializer.serialize(@product)}"
+  end
+
   private
+
+  def current_product
+    @product = Product.find(params["id"])
+  end
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
