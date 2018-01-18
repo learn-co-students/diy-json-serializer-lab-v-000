64324fbdacafa031f054e32f75fc4b58264ab40f
kyleblee <hello.kylelee@gmail.com>
2018-01-17 21:52:48 -0500

    lab complete

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,6 @@
 $(function() {
   $('.js-next').on('click', function() {
     var nextId = parseInt($('.js-next').attr('data-id')) + 1;
-    debugger;
     $.getJSON('/products/' + nextId + '/data', function(data) {
       $('.productName').text(data["name"]);
       $('.productDescription').text(data["description"]);
