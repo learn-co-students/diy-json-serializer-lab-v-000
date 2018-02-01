fd82e71074dc5e092501ba396f50ecd5c2e813c6
mandlerm <amaya.melissa@gmail.com>
2018-01-31 20:37:30 -0600

    Done.

---
+++ app/serializer/product_serializer.rb
@@ -0,0 +1,18 @@
+class ProductSerializer
+  def self.serialize(product)
+
+    # start with the open brace to create a valid JSON object
+    serialized_post = '{'
+
+    serialized_post += '"id": ' + product.id.to_s + ', '
+    serialized_post += '"name": "' + product.name + '", '
+    serialized_post += '"price": ' + product.price.to_s + ', '
+
+    # the author association can also be represented in JSON
+    serialized_post += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_post += '"description": "' + product.description + '"'
+
+    # and end with the close brace
+    serialized_post += '}'
+  end
+end
