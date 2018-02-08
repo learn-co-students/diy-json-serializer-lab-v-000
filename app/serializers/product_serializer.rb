6a9b1e6994a6040b9f037b2606c0370562783657
streamedLine <dovidlwtn@gmail.com>
2018-02-08 00:48:01 -0500

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,11 +1,11 @@
 class ProductSerializer
 	def self.serialize(product)
 		json = '{'
-			json += '"id": ' + product.id.to_s + ','
-			json += '"name": ' + '"' + product.name + '",'
-			json += '"price": ' + product.price.to_s + ','
-			json += '"inventory": ' +  product.inventory.to_s + ',' 
-			json += '"description": ' + '"' + product.description + '"'
+			# json += '"id": ' + product.id.to_s + ','
+			json += '"name": "' + product.name + '",'
+			json += '"price": ' + product.price.to_s + ',' unless product.price.nil?
+			json += '"inventory": ' + product.inventory.to_s + ',' unless product.inventory.nil?
+			json += '"description": "' + product.description + '"' unless product.description.nil?
 		json += '}'
 	end
 end
