1450b17a690c2949af5da3e0b5900afc89ad286e
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-02-19 19:26:41 +0000

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -8,7 +8,7 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
     serialized_product += '"price": "' + product.price.to_s + '", '
-    serialized_product += '"inventory": "' + product.inventory.to_s + '"'
+    serialized_product += '"inventory": ' + product.inventory.to_s
  
     # the author association can also be represented in JSON
     #serialized_product += '"author": {'
