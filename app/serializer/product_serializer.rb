af4d06c91748f1f02b14a181ffa2dca9a5809672
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-12 04:15:19 +0000

    Done.

---
+++ app/serializer/product_serializer.rb
@@ -0,0 +1,15 @@
+# product's attributes (name, price, inventory and description) to JSON.
+class ProductSerializer
+  def self.serialize(product)
+    serialized_product = '{'
+
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"price": "' + product.price.to_s + '", '
+
+    # and end with the close brace
+    serialized_product += '}'
+  end
+end
