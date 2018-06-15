afa3a9a50846623a65d82f436a84bd36d3696c95
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-15 15:04:11 -0400

    Done.

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,15 @@
+class ProductSerializer
+  require 'pry'
+  def self.serialize(product)
+
+    # start with the open brace to create a valid JSON object
+    serialized_post = '{'
+
+    serialized_post += '"id": ' + product.id.to_s + ', '
+    serialized_post += '"name": "' + product.name + '", '
+    serialized_post += '"price": ' + product.price.to_s + ', '
+    serialized_post += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_post += '"description": "' + product.description + '"}'
+
+  end
+end
