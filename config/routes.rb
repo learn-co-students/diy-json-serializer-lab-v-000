b2d60b3dab32a22fbeba92de1785b63c558c5831
Olena Ageyeva <eageeva@hotmail.com>
2018-06-29 20:47:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
     get 'products/:id', to: 'products#show'
   get 'products/:id/post_data', to: 'products#post_data'
-  get 'products/:id/datd', to: 'products#data'
+  get 'products/:id/data', to: 'products#data'
 end
