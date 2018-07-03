2ad73d4b119f98a00dae23129be49f7b097473a5
ronald vilorio <soccer23415@gmail.com>
2018-07-03 18:29:12 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,10 @@ class ProductsController < ApplicationController
     Product.create(product_params)
     redirect_to products_path
   end
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
 
   private
 
