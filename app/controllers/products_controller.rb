d8da2d145e04b74da680070c02e0cda257a14565
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-17 19:40:54 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,9 +18,19 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
+    @product = Product.create(product_params)
     redirect_to products_path
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
 
   private
 
