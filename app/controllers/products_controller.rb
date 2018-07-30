e5f0a20014558ea5d418f373f4a30a3bb45ea9b8
AspenJames <aspenjames@tqca.org>
2018-07-30 12:31:12 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,16 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+  def data
+    json = ProductSerializer.serialize(Product.find(params[:id]))
+    render json: json
+  end
+
   private
 
   def product_params
