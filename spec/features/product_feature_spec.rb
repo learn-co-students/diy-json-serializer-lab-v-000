75a32abae31a1a8e49b32c914208d434a2b7f2df
fonzmp <fonzmp@gmail.com[color]>
2018-08-09 12:14:42 -0400

    Done.

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Products", type: :feature do
 
   it 'requires javascript to go next' do
     p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
-    p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
+    p2 = Product.create!(name: "Test Product Two", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
 
     visit product_path(p1)
     expect(page).to have_content p1.name
