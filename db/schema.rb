981deaecd6a02d691b79998e433a040f0e12d5cb
Brad Carlson <b2carlso@yahoo.com>
2018-08-15 22:45:23 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221021155) do
+ActiveRecord::Schema.define(version: 20180815223736) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
