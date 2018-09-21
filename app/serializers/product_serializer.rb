d12a4ff43789a953acbcac5714dc07aeba517f7c
barbara.prusiewicz prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-21 04:48:55 +0000

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -7,7 +7,7 @@ class ProductSerializer
       serialized_product += '"id": ' + product.id.to_s + ', '
       serialized_product += '"name": "' + product.name + '", '
       serialized_product += '"description": "' + product.description + '", '
-       serialized_product += '"inventory": "' + product.inventory
+      serialized_product += '"inventory": ' + product.inventory.to_s
       # and end with the close brace
       serialized_product += '}'
     end
