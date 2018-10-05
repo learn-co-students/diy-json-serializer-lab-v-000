3bcee59b7876ffe082dcdbcec8352cec1b99d517
AubreyNeimeier <neimeier.1@osu.edu>
2018-10-05 12:20:30 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,16 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
   def inventory
     product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
