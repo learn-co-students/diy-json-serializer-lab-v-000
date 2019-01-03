e2d2feaeface3343b786c9ab42ae784a65a799a7
Jayon Kim <jpkim921@gmail.com>
2019-01-03 00:28:56 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :new, :create]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
+  get 'products/:id/data', to: 'products#data'
+
 end
