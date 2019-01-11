de9752de53a28388a5c202ff9d56811635dfffbf
ElleeB <laura.trager@gmail.com>
2019-01-11 13:47:36 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
-    render plain: ProductSerializer.serialize(product)
+    render json: ProductSerializer.serialize(product)
   end
 
   def new
