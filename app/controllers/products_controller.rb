c8719f16d0a7941729ab567e597a7c27c8f7fca7
Richard Sanchez <richard.charles.s@gmail.com>
2019-03-13 17:34:03 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,5 @@
+
+
 class ProductsController < ApplicationController
   def index
     @products = Product.all
@@ -20,6 +22,15 @@ class ProductsController < ApplicationController
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
