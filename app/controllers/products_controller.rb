d3278bc49cc7620830ae12a514e6e980b9e30d8b
github username <github email address>
2019-03-23 14:09:17 -0400

    Show next product when link is clicked

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,41 +1,43 @@
 class ProductsController < ApplicationController
-  def index
-    @products = Product.all
-  end
+    #before_action :set_post, only: [:show, :description, :inventory]
+    def index
+        @products = Product.all
+    end
 
-  def inventory
-    product = set_product
-    render plain: product.inventory.to_i!=0
-  end
+    def inventory
+        product = set_product
+        render plain: product.inventory.to_i!=0
+    end
 
-  def description
-    product = set_product
-    render plain: product.description
-  end
+    def description
+        product = set_product
+        render plain: product.description
+    end
 
-  def new
-    @product = Product.new
-  end
+    def new
+        @product = Product.new
+    end
 
-  def create
-    Product.create(product_params)
-    redirect_to products_path
-  end
+    def create
+        Product.create(product_params)
+        redirect_to products_path
+    end
 
-  def show
+    def show
+        @product = Product.find(params[:id])
+    end
 
-  end
+    def data
+        product = set_product
+        render json: ProductSerializer.serialize(product)
+    end
 
-  def data
-    render json: ProductSerializer.serialize(set_product)
-  end
+    private
+    def set_product
+        @product = Product.find(params[:id])
+    end
 
-  private
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
-  def product_params
-    params.require(:product).permit(:name, :description, :inventory, :price)
-  end
+    def product_params
+        params.require(:product).permit(:name, :description, :inventory, :price)
+    end
 end
