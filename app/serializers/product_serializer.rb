bc6316c1ad11ba1dd414ed99931a8ec3bceb3257
ryanwevans <ryanwevans@icloud.com>
2019-04-06 16:50:51 -0700

    Done.

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,10 @@
+class ProductSerializer
+  def self.serialize(product)
+    serialized_product = '{'
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"price": ' + product.price + ', '
+    serialized_product += '"inventory": "' + product.inventory + '", '
+    serialized_product += '"description": "' + product.description + '" '
+    serialized_product += '}'
+  end
+end
