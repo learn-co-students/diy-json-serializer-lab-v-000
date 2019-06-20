65b54c7d9db17d0b60cae3e3b9724491db18b134
brennanmrose <brennan.m.rose@gmail.com>
2019-06-20 08:35:09 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def inventory
@@ -22,9 +27,15 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
   end
+
 end
